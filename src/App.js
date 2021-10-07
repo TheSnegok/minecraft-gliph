ea92633dc9b066890474919b091d61c1c327ff78
Max <lord180499@gmail.com>
2021-10-07 13:47:19 +0300

    finally

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Gliph from "./component/Gliphs/Gliph";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [value, setValue] = useState("");
+	const clear = () => {
+		setValue("");
+	};
+	const clearLast = () => {
+		setValue(value.slice(0, value.length - 1));
+	};
+	const space = () => {
+		setValue(value + " ");
+	};
+	const validation = (e) => {
+		const regExp = /^[a-zA-Z|\s]+$/;
+		setValue(
+			e.target.value === ""
+				? e.target.value
+				: e.target.value.match(regExp) === null
+				? value + ""
+				: e.target.value.match(regExp).input
+		);
+	};
+	return (
+		<div className="App">
+			<main>
+				<div className="gliph">
+					<div className="gliphArea">
+						<div className="wrapperHeading">
+							<h1>Gliphs</h1>
+						</div>
+						<input
+							className="inputLine gliphs"
+							value={value}
+							readOnly
+						/>
+						<div className="controlPanel">
+							<button className="btn" onClick={clear}>
+								Clear
+							</button>
+							<button className="btn" onClick={clearLast}>
+								Delete last
+							</button>
+							<button className="btn" onClick={space}>
+								Space
+							</button>
+						</div>
+						<div className="gliphAlphabet">
+							<Gliph value={value} set={setValue} />
+						</div>
+					</div>
+				</div>
+			</main>
+			<div className="history">
+				<div className="wrapperHeading">
+					<h1>Translation</h1>
+				</div>
+				<div className="centry">
+					<input
+						type="text"
+						value={value}
+						className="inputLine translate"
+						onChange={validation}
+					/>
+				</div>
+			</div>
+			<a href="mailto:lord180499@gmail.com">
+				<div className="wrapperCreator">created Max Snega</div>
+			</a>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/component/Gliphs/Gliph.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+const Gliph = ({ value, set }) => {
+    const handleClick = (letter) => {
+        set(value + letter);
+    }
+    return (
+        <>
+            {alphabet.map(l => <div key={l} className="gliphMine" onClick={() => handleClick(l)}>{l}</div>)}
+        </>
+    )
+}
+
+export default Gliph
